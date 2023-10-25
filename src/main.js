16fee8ec13a209bdb671b8bb404143ccd643c124
Elina Sankila <sankilaelina@gmail.com>
2023-10-25 10:46:21 +0300

    tehtävä aloitettu

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log("REQUEST", req.method, req.url);
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Welcome to my REST API!</h1>");
 });
