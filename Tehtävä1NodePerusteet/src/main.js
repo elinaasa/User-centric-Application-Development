27cfa8865e0cf6a9c24126bffe7b9cb63b265d0d
Elina Sankila <sankilaelina@gmail.com>
2023-10-26 11:25:59 +0300

    esim

--- Tehtävä1NodePerusteet/src/main.js
+++ Tehtävä1NodePerusteet/src/main.js
@@ -1,31 +1,30 @@
-// index.js
 import http from "http";
 import { getItems, getItemsById, postItem } from "./items.js";
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log("REQUEST", req.method, req.url);
+  console.log("request", req.method, req.url);
   const { method, url } = req;
   const reqParts = url.split("/");
-
+  // check method, url and generate response accordingly (=routing)
   if (method === "GET" && url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1> Welcome to my API</h1>");
-    res.write("<p> Welcome to my API<p>");
+    res.write("<h1>Welcome to my API</h1>");
+    res.write("<p>documentation comes here</p>");
     res.end();
   } else if (method === "GET" && reqParts[1] === "items" && reqParts[2]) {
-    console.log("GETting items with id", reqParts[2]);
+    console.log("GETting item with id", reqParts[2]);
     getItemsById(res, reqParts[2]);
   } else if (method === "GET" && reqParts[1] === "items") {
-    console.log("GETting items");
+    console.log("GETting all items");
     getItems(res);
   } else if (method === "POST" && reqParts[1] === "items") {
-    console.log("POSting new items");
-    postItem(res);
+    console.log("POSTing a new item");
+    postItem(req, res);
   } else {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end("<h1>Welcome to my REST API!</h1>");
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end('{"message": "404 Resource not found!"}');
   }
 });
 
