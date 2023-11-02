a8ed44145643f9a51b25a0bea2ca17c12166c6a3
Elina Sankila <sankilaelina@gmail.com>
2023-11-02 15:54:38 +0200

    mitä

---
+++ KäyttäjäkeskeinenSovelluskehitys/ExpressSovelluskehys/Assignment2/src/index.js
@@ -0,0 +1,15 @@
+// Implement a simple REST API with Express framework:
+
+//     Use Pug as a template engine for basic HTML landing page GET / (1 point)
+//     Implement a REST API by following this API reference with the mock data included in it (max. 4 points)
+
+//     Continue with the project you created in the previous assignment and create a new branch express for this assignment.
+//         use the existing code as a starting point or generate a new project with npm init
+//     Install Express framework and Pug template engine
+//     Render a dynamic HTML page with Express and Pug
+//         provide some information about your API, use dynamic content rendering
+//     Implement API endpoints following the API documentation and using the mock data provided
+//         note that API documentation is not complete, you need to figure out missing details. for example add correct status codes and corresponding messages or data to the responses
+//     Serve static media files from src/media folder
+//         use e.g. some small-sized images from Internet
+//         match the filenames with the mock data
