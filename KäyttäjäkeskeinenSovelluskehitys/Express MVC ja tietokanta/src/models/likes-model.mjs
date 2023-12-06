345b17adbd31fe8b7817e7e48933e5c95a6752df
Elina <sankilaelina@gmail.com>
2023-12-06 18:44:20 +0200

    express mvc

--- KäyttäjäkeskeinenSovelluskehitys/Express MVC ja tietokanta/src/models/likes-model.mjs
+++ KäyttäjäkeskeinenSovelluskehitys/Express MVC ja tietokanta/src/models/likes-model.mjs
@@ -1,15 +1,15 @@
-import {promisePool} from "../utils/database.mjs";
+import { promisePool } from "../utils/database.mjs";
 
 const addLike = async (user_id, media_id) => {
   try {
     const sql = `INSERT INTO Likes (user_id, media_id)
-                VALUES (?, ?)`;               
+                VALUES (?, ?)`;
     const params = [user_id, media_id];
     const result = await promisePool.query(sql, params);
     return result[0].insertId;
   } catch (e) {
-    console.error('error', e.message);
-    return {error: e.message};
+    console.error("error", e.message);
+    return { error: e.message };
   }
 };
 
@@ -19,53 +19,48 @@ const fetchLikesByMediaId = async (media_id) => {
     WHERE media_id = ?
   `;
 
-  // user level id defaults to 2 (normal user)                 
+  // user level id defaults to 2 (normal user)
   const params = [media_id];
 
   try {
     const result = await promisePool.query(sql, params);
     return result[0];
   } catch (e) {
-    console.error('error', e.message);
-    return {error: e.message};
+    console.error("error", e.message);
+    return { error: e.message };
   }
-}
+};
 
 const fetchLikesByUserId = async (user_id) => {
   const sql = `
     SELECT * FROM Likes
     WHERE user_id = ?
-  `;                
+  `;
   const params = [user_id];
 
   try {
     const result = await promisePool.query(sql, params);
     return result[0];
   } catch (e) {
-    console.error('error', e.message);
-    return {error: e.message};
+    console.error("error", e.message);
+    return { error: e.message };
   }
-}
+};
 
 const deleteLikeById = async (id) => {
   const sql = `
     DELETE FROM Likes
     WHERE like_id = ?;
-  `;               
+  `;
   const params = [id];
 
   try {
     const result = await promisePool.query(sql, params);
     return result[0];
   } catch (e) {
-    console.error('error', e.message);
-    return {error: e.message};
+    console.error("error", e.message);
+    return { error: e.message };
   }
-}
+};
 
-export {
-  addLike,
-  fetchLikesByMediaId,
-  fetchLikesByUserId,
-  deleteLikeById
-}
+export { addLike, fetchLikesByMediaId, fetchLikesByUserId, deleteLikeById };
